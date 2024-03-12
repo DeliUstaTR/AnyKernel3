0379e7f6c32e10ddfc9cab4f59940bec18f95fcd
Salih <91677076+DeliUstaTR@users.noreply.github.com>
2024-03-12 11:48:18 +0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Sapphire for Ginkgo | @Flopster101
+kernel.string=Sapphire for Ginkgo
 do.devicecheck=1
 do.modules=0
 do.systemless=1
